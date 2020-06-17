71e44feacd4864a296d0f8da922cefc4a92da132
CristianOmarSandovalAceves <zombienaitor@gmail.com>
2020-06-17 12:15:32 -0500

    Botones de fechas hechos

--- app/src/main/java/com/perspective/inszap/Reporte1.java
+++ app/src/main/java/com/perspective/inszap/Reporte1.java
@@ -1,20 +1,22 @@
 package com.perspective.inszap;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
-public class Reporte1 extends AppCompatActivity {
- private Button btnfecha,btnfecha2;
+public class Reporte1 extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+ private Button btnfecha,btnfecha2,botonaceptar;
  private EditText ediF1,edif2;
+ private int dayf,monthf,yearf,boton1,boton2;
+
+ private String fecha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +24,19 @@ public class Reporte1 extends AppCompatActivity {
 
       this.btnfecha=(Button)findViewById(R.id.btnFecha1);
       this.btnfecha2=(Button)findViewById(R.id.btnFecha2);
+      this.botonaceptar=(Button)findViewById(R.id.aceptarF);
       this.ediF1=(EditText)findViewById(R.id.etfecham1);
       this.edif2=(EditText)findViewById(R.id.etfecham2);
-
-
+      this.boton1=0;
+      this.boton2=0;
+      final DataPickerDialogFragment p= new DataPickerDialogFragment();
       btnfecha2.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
 
+              p.show(getSupportFragmentManager(),"data picker");
+              //updateDisplay1(edif2);
+              boton2=1;
           }
       });
 
@@ -37,6 +44,16 @@ public class Reporte1 extends AppCompatActivity {
           @Override
           public void onClick(View v) {
 
+             p.show(getSupportFragmentManager(),"data picker");
+             boton1=1;
+             //updateDisplay1(ediF1);
+          }
+      });
+
+      botonaceptar.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              
           }
       });
 
@@ -44,5 +61,36 @@ public class Reporte1 extends AppCompatActivity {
 
 
 
+
+
+
     }
+    private void updateDisplay1( EditText v) {
+
+      v.setText(fecha);
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(year,monthOfYear,dayOfMonth);
+
+        //String currentString= DateFormat.getDateInstance().format(dayOfMonth+"/"+month+"/"+year);
+        //ediF1.setText(currentString);
+        /*yearf=year;
+        dayf=dayOfMonth;
+        monthf=monthOfYear;*/
+
+
+        fecha= dayOfMonth+"/"+monthOfYear+"/"+year;
+        if (boton2==1 && boton1==0)
+        updateDisplay1(edif2);
+        boton2=0;
+        if(boton2==0 && boton1==1)
+            updateDisplay1(ediF1);
+        boton1=0;
+    }
+
+
 }
